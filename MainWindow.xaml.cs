bd08cadf78bcd899c4cec69a9dd31eb1fc717c69
IanMcT <ian.mctavish@tldsb.on.ca>
2019-05-01 06:20:27 -0400

    Update MainWindow.xaml.cs
    Added gamestate, timer and player.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,17 +15,51 @@ using System.Windows.Shapes;
 
 namespace qbert
 {
+
+    public enum GameState { Menu, ScoreScreen, GameOn, GameOver, movingToGame }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Global Variables
         Pyramid pyramid;
+        public GameState gameState;
+        public System.Windows.Threading.DispatcherTimer gameTimer;
+        public Player player;
+
+        /// <summary>
+        /// Constructor - set all initial values
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
-            pyramid = new Pyramid(canvas);
-            pyramid.draw();
-        }
-    }
-}
+            gameState = GameState.movingToGame;
+            gameTimer =  new System.Windows.Threading.DispatcherTimer(); ;
+            //start Timer
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);//fps
+            gameTimer.Start();
+
+            
+            
+        }//end MainWindow
+
+        /// <summary>
+        /// Runs for every frame (i.e. fps), controls everything.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            if (gameState == GameState.movingToGame)
+            {
+                pyramid = new Pyramid(canvas);
+                pyramid.draw();
+                gameState = GameState.GameOn;
+                player = new Player(pyramid, canvas);
+            }//done movingtogame
+            else if (gameState == GameState.GameOn) { }//done GameOn
+        }//end GameTimer_Tick
+    }//end Class
+}//end Namespace
